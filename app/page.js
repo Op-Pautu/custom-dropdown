87cafc57bb895be0a77d660f855674bd452507dc
Op-Pautu <pautuop@gmail.com>
2024-02-24 11:09:35 +0530

    refactor

--- app/page.js
+++ app/page.js
@@ -1,24 +1,17 @@
 import DropDown from "@/components/DropDown";
-import RadixDropDown from "@/components/RadixDropDown";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+    <main className="flex flex-col items-center justify-between min-h-screen gap-16 p-24">
       <DropDown
-        id="1"
-        company="Op Test Company"
-        img="/empty.png"
-        isOpen={false}
-      />
-      {/* <DropDown
         label="test"
         organization="op org"
         id="2"
         company="Op Test Company2"
         img="/test.png"
+        side="top"
         isOpen={false}
-      /> */}
-      {/* <RadixDropDown /> */}
+      />
     </main>
   );
 }
--- components/DropDown.jsx
+++ components/DropDown.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { useEffect, useRef, useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
@@ -9,48 +10,85 @@ const DropDown = ({
   options,
   label,
   onSelect,
-  isOpen,
+  isOpen: propIsOpen,
   className,
   organization,
   menuClassName,
   itemClassName,
   menuContent,
   company,
+  side,
+
   img,
   ...otherProps
 }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(isOpen || false);
+  const [isMenuOpen, setIsMenuOpen] = useState(propIsOpen || false);
+  const dropdownRef = useRef(null);
+  const [dropdownSide, setDropdownSide] = useState(side);
 
   useEffect(() => {
-    console.log({ id });
-    const closeOnOutsideClick = (event) => {
-      const targetElement = event.target;
-      const dropdownElement = document.getElementById(`dropdown-menu-${id}`);
-      console.log({ targetElement });
-      console.log({ dropdownElement });
-      if (dropdownElement && !dropdownElement.contains(targetElement)) {
+    const toggleBodyOverflow = () => {
+      document.body.style.overflow = isMenuOpen ? "hidden" : "auto";
+    };
+
+    toggleBodyOverflow();
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isMenuOpen]);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      event.stopPropagation();
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        !event.target.closest(`.dropdown-menu-${id}`)
+      ) {
         setIsMenuOpen(false);
       }
     };
-    if (isMenuOpen) {
-      if (isMenuOpen) {
-        document.addEventListener("click", closeOnOutsideClick);
-      } else {
-        document.removeEventListener("click", closeOnOutsideClick);
-      }
 
-      return () => document.removeEventListener("click", closeOnOutsideClick);
-    }
-  }, [isMenuOpen, id]);
+    const handleKeyBoardClick = (event) => {
+      const key = event.key;
+      if (key === "Escape") {
+        setIsMenuOpen(false);
+      }
+    };
+    const updateDropdownPosition = () => {
+      if (dropdownRef.current) {
+        const dropdownRect = dropdownRef.current.getBoundingClientRect();
+        const viewportHeight = window.innerHeight;
+
+        const shouldRenderAtTop = dropdownRect.bottom > viewportHeight / 2;
+
+        setDropdownSide(shouldRenderAtTop ? "top" : "bottom"); // Update side
+      }
+    };
+
+    updateDropdownPosition();
+
+    window.addEventListener("scroll", updateDropdownPosition);
+    window.addEventListener("resize", updateDropdownPosition);
+    document.addEventListener("keydown", handleKeyBoardClick);
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyBoardClick);
+      window.removeEventListener("scroll", updateDropdownPosition);
+      window.removeEventListener("resize", updateDropdownPosition);
+    };
+  }, [id]);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <div className="min-w-[250px]">
+    <div className="relative" ref={dropdownRef}>
       <div
-        className="flex items-center gap-3 px-4 py-2 text-white rounded-md cursor-pointer bg-sky-400"
+        className=" min-w-[250px] flex items-center gap-3 px-4 py-2 text-white rounded-md cursor-pointer bg-sky-400"
         onClick={toggleMenu}
       >
         <p>{label ? label : "Test"}</p>
@@ -66,12 +104,13 @@ const DropDown = ({
       </div>
       {isMenuOpen && (
         <div
-          className="z-50 mx-auto bg-white shadow-md min-w-[250px] mt-4 px-6 py-4 rounded-md backdrop-blur-lg"
-          id={`dropdown-menu-${id}`}
+          className={cn(
+            "dropdown-menu z-50 absolute mx-auto bg-white shadow-md min-w-[250px] mt-4 px-6 py-4 rounded-md backdrop-blur-lg",
+            dropdownSide === "bottom" ? "top-10" : "bottom-12"
+          )}
         >
           <div>
             <div className="flex gap-3">
-              {/* Customizable image */}
               <img
                 width={50}
                 height={40}
@@ -80,7 +119,6 @@ const DropDown = ({
                 className="rounded-md"
               />
               <div className="flex flex-col">
-                {/* Customizable organization name */}
                 <p className="font-semibold">My Organization</p>
                 <p className="text-gray-500">
                   {company ? company : "Test Org"}
@@ -90,9 +128,9 @@ const DropDown = ({
           </div>
           <div className="pt-10">
             <h1>Subscription</h1>
-            <p className="text-neutral-500">Expire On: 30 Jun 2024</p>
-            <p className="text-neutral-500">Credits: 9999</p>
-            <p className="text-neutral-500">Messages Available: 5910</p>
+            <p className="text-sm text-neutral-500">Expire On: 30 Jun 2024</p>
+            <p className="text-sm text-neutral-500">Credits: 9999</p>
+            <p className="text-sm text-neutral-500">Messages Available: 5910</p>
           </div>
           <button className="px-4 py-2 mt-4 text-white bg-blue-600 rounded-md">
             Sign Out
